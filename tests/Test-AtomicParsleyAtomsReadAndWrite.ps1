9eafe3c33746aa7c7f732e16cfe6b2b1e123b0b5
seabopo <seabopo@icloud.com>
2025-07-19 23:44:25 -0500

    update the tests to compare pre-post change atoms

--- tests/Test-AtomicParsleyAtomsReadAndWrite.ps1
+++ tests/Test-AtomicParsleyAtomsReadAndWrite.ps1
@@ -5,6 +5,13 @@
 #==================================================================================================================
 
 #==================================================================================================================
+# Set Test Variables
+#==================================================================================================================
+
+$testFileTypes = @('.mp4', '.m4v')
+
+
+#==================================================================================================================
 # Initialize Test Environment
 #==================================================================================================================
 
@@ -12,7 +19,7 @@ Clear-Host
 
 $ErrorActionPreference = "Stop"
 
-$env:PS_STATUSMESSAGE_VERBOSE_MESSAGE_TYPES = '["Header","Process","Information","Debug"]'
+$env:PS_STATUSMESSAGE_VERBOSE_MESSAGE_TYPES = '["Process","Information","Debug"]'
 $env:PS_STATUSMESSAGE_SHOW_VERBOSE_MESSAGES = $false
 
 Set-Location  -Path $PSScriptRoot
@@ -41,7 +48,10 @@ Import-Module $modulePath  -Force
 #
 #==================================================================================================================
 
+Write-Msg -h -ps -b -m $( ' Running AtomicParsley Read/Write Tests' )
+
 Get-ChildItem $mediaPath -Recurse -File | 
+    Where-Object {  $_.Extension -in $testFileTypes -and $_.Name -notlike '*[X]*' } |
     Foreach-Object {
 
         $path       = $_.FullName
@@ -52,7 +62,7 @@ Get-ChildItem $mediaPath -Recurse -File |
         $targetPath = Join-Path -Path $directory -ChildPath $targetName
         $shouldCopy = $name.endsWith(' [X]') ? $false : $true
 
-        Write-Msg -s -ps -m $( 'Processing Source file: {0}' -f $path )
+        Write-Msg -a -ps -m $( 'Processing Source file: {0}' -f $path )
 
         if ( $shouldCopy -and -not (Test-Path -LiteralPath $targetPath) ) {
             Write-Msg -a -il 1 -m $( 'Test file NOT found: {0}' -f $path )
@@ -64,9 +74,51 @@ Get-ChildItem $mediaPath -Recurse -File |
             Write-Msg -a -il 1 -m $( 'Test file exists: {0}' -f $targetPath )
         }
 
-        $atoms = Read-AtomicParsleyAtoms -File $path -SaveToFile
-        Write-AtomicParsleyAtoms -File $targetPath -Atoms $atoms -RemoveAll
-        Read-AtomicParsleyAtoms -File $targetPath -SaveToFile | Out-Null
+        Write-Msg -a -il 1 -m $( 'Reading atoms from source file ...' )
+        $sourceAtoms = Read-AtomicParsleyAtoms -File $path -SaveToFile
+        Write-Msg -a -il 2 -m $( 'Complete.' )
+
+        Write-Msg -a -il 1 -m $( 'Cleaning file and writing atoms ...' )
+        Write-AtomicParsleyAtoms -File $targetPath -Atoms $sourceAtoms -RemoveAll | Out-Null
+        Write-Msg -a -il 2 -m $( 'Complete.' )
+
+        Write-Msg -a -il 1 -m $( 'Reading atoms from test file ...' )
+        $targetAtoms = Read-AtomicParsleyAtoms -File $targetPath -SaveToFile 
+        Write-Msg -a -il 2 -m $( 'Complete.' )
+
+        Write-Msg -a -il 1 -m $( 'Comparing atoms ...' )
+        foreach ( $atom in $sourceAtoms.keys ) {
+            Write-Msg -a -il 2 -m $( 'Checking atom: {0}' -f $atom)
+            if ( $atom -eq 'RawAtomData' ) {
+                Write-Msg -a -il 3 -m $( 'Skipping RawAtomData atom.' )
+            }
+            else {
+                Write-Msg -a -il 3 -m $( 'Source value: {0}'  -f $sourceAtoms[$atom])
+                if ( $targetAtoms.ContainsKey( $atom ) ) {
+                    Write-Msg -a -il 3 -m $( 'Target value: {0}' -f $targetAtoms[$atom])
+                    if ( $targetAtoms[$atom] -eq $sourceAtoms[$atom] ) {
+                        Write-Msg -a -il 2 -m $( 'Atoms match.' )
+                    }
+                    else {
+                        Write-Msg -e -il 2 -m $( 'Atoms do NOT match.' )
+                    }
+                }
+                else {
+                    $testAtom = $AP_ATOMS | Where-Object { $_.PropertyName -eq $atom }
+                    if ( [String]::IsNullOrEmpty($testAtom) ) {
+                    Write-Msg -e -il 3 -m $( 'Source atom is not defined in the atoms data file.' )
+                    }
+                    else {
+                        if ( $testAtom.WriteSupported ) {
+                            Write-Msg -e -il 3 -m $( 'Target value: missing' )
+                        }
+                        else {
+                            Write-Msg -w -il 3 -m $( 'Atomic parsley does not support writing this atom.' )
+                        }
+                    }
+                }
+            }
+        }
 
     }
 
